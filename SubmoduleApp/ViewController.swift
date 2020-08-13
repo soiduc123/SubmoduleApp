dfbb3f18c1b20c67395f8cfa39976a5629bfd426
Thinh Dao <dnnthinh@tma.com.vn>
2020-08-13 09:10:48 +0700

    Initial Commit

---
+++ SubmoduleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SubmoduleApp
+//
+//  Created by Thinh Dao on 8/13/20.
+//  Copyright © 2020 Thinh Dao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
